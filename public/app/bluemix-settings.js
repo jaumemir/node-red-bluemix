e19c6f90ee82fb383842174a858efef8ce339f1c
jaume <jaume.miralles@es.ibm.com>
2015-09-22 22:29:25 +0200

    Added [ osModule:require('os') ] to bluemixs-ettings

--- public/app/bluemix-settings.js
+++ public/app/bluemix-settings.js
@@ -49,7 +49,9 @@ var settings = module.exports = {
     // Serve up the welcome page
     httpStatic: path.join(__dirname,"public"),
 
-    functionGlobalContext: { },
+    functionGlobalContext: { 
+    	osModule:require('os') 
+    },
 
     storageModule: require("./couchstorage")
 }
